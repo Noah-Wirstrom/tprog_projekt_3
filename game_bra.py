47c4a044c314f42721ce699166330ed33c8005f4
Noah-Wirstrom <noah.wirstrom@elev.ga.ntig.se>
2024-05-23 14:30:51 +0200

    Update game_bra.py

--- game_bra.py
+++ game_bra.py
@@ -9,17 +9,19 @@ from turtle import speed
 import pygame
 from pygame.locals import *
 
+# Skapa en spelklass
 class Game:
     def __init__(self):
         pygame.init()
 
+        # Skärmstorlek och andra konstanter
         self.screen_width = 700
         self.screen_height = 500
-        ground_image_size = (self.screen_width,100)
+        ground_image_size = (self.screen_width, 100)
         bg_image_size = (self.screen_width, self.screen_height)
         sprite_size = (50, 50)
-        self.sprite_x = self.screen_width/3
-        self.sprite_y = self.screen_height/2
+        self.sprite_x = self.screen_width / 3
+        self.sprite_y = self.screen_height / 2
         
         self.pipe_size_y = 350
         self.pipe_gap = 200
@@ -35,51 +37,56 @@ class Game:
         self.fps = 60
         self.clock = pygame.time.Clock()
 
-        self.i=0
+        self.i = 0
         
+        # Skapa grupper för sprites
         self.all_sprites = pygame.sprite.LayeredUpdates()
         self.player_sprite = pygame.sprite.LayeredUpdates()
         self.pipe_sprite = pygame.sprite.LayeredUpdates()
        
         self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
         pygame.display.set_caption('Flappig Fågel')
-        #laddar bilder
+        
+        # Ladda bilder
         bg = pygame.image.load('img/bg_fancy.png') 
         ground_image = pygame.image.load('img/ground_image_1.png')
         sprite = pygame.image.load('img/game_sprite.png')
-        start_screen =  pygame.image.load('img/start_screen.png')
-        end_screen =  pygame.image.load('img/game_over_screen.png')
+        start_screen = pygame.image.load('img/start_screen.png')
+        end_screen = pygame.image.load('img/game_over_screen.png')
         pipe = pygame.image.load('img/better_pipe.png')
-        #skalar bilder
+
+        # Skala bilder
         self.bg = pygame.transform.scale(bg, bg_image_size)
         self.ground_image = pygame.transform.scale(ground_image, ground_image_size)
-        self.sprite = pygame.transform.scale(sprite, sprite_size )
-        self.end_screen = pygame.transform.scale(end_screen,(self.screen_width,self.screen_height))
-        self.start_screen = pygame.transform.scale(start_screen,(self.screen_width,self.screen_height))
-        self.pipe = pygame.transform.scale(pipe,(50 ,self.pipe_size_y)) 
+        self.sprite = pygame.transform.scale(sprite, sprite_size)
+        self.end_screen = pygame.transform.scale(end_screen, (self.screen_width, self.screen_height))
+        self.start_screen = pygame.transform.scale(start_screen, (self.screen_width, self.screen_height))
+        self.pipe = pygame.transform.scale(pipe, (50, self.pipe_size_y))
 
-    def draw(self,):
+    # Metod för att rita spelet
+    def draw(self):
         if self.pipe_new:
-           
-            pipe_spawn = random.randint(175 , 430)
+            # Slumpmässigt generera nya rör
+            pipe_spawn = random.randint(175, 430)
             Pipe(self, 700, pipe_spawn)
             Pipe(self, 700, pipe_spawn - self.pipe_gap - self.pipe_size_y)
             self.pipe_new = False
- 
-        self.screen.blit(self.bg, (0,0))
-        self.screen.blit(self.ground_image, (self.i,450))
-        self.screen.blit(self.ground_image,(self.screen_width+self.i,450))
-        self.all_sprites.draw(self.screen)
-        if (self.i==-self.screen_width):
-            self.screen.blit(self.ground_image,(self.screen_width+self.i,450) )
-            self.i=0
-        self.i-=2
 
-        self.screen.blit(self.font.render("SCORE:" + str(self.score//2), True, (255,255,0)), (self.screen_width/2-60, 10))
+        self.screen.blit(self.bg, (0, 0))
+        self.screen.blit(self.ground_image, (self.i, 450))
+        self.screen.blit(self.ground_image, (self.screen_width + self.i, 450))
+        self.all_sprites.draw(self.screen)
+        if (self.i == -self.screen_width):
+            self.screen.blit(self.ground_image, (self.screen_width + self.i, 450))
+            self.i = 0
+        self.i -= 2
+
+        self.screen.blit(self.font.render("SCORE:" + str(self.score // 2), True, (255, 255, 0)), (self.screen_width / 2 - 60, 10))
 
         self.clock.tick(self.fps)
         pygame.display.update()
 
+    # Starta ett nytt spel
     def new(self):
         Player(self, 100, 100)
         Pipe(self, 700, 350)
@@ -87,162 +94,144 @@ class Game:
         self.score = 0
         self.play = True
 
+    # Hantera händelser
     def event(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.play = False
                 self.run = False
 
+    # Spelhuvudloop
     def main(self):
         while self.play:
             self.event()
             self.update()
             self.draw()
-        
 
+    # Uppdatera alla sprites
     def update(self):
         self.all_sprites.update()
 
+    # Startskärm
     def start(self):
-       intro = True
-       while intro:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                intro = False
-                self.run = False
-        self.screen.blit(self.start_screen, (0,0))
-        if pygame.key.get_pressed()[pygame.K_SPACE]:
-              intro = False 
-        self.clock.tick(self.fps)
-        pygame.display.update()
+        intro = True
+        while intro:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    intro = False
+                    self.run = False
+            self.screen.blit(self.start_screen, (0, 0))
+            if pygame.key.get_pressed()[pygame.K_SPACE]:
+                intro = False 
+            self.clock.tick(self.fps)
+            pygame.display.update()
 
-    
-
+    # Slutskärm
     def end(self):
-        Restart_button = Button(self.screen_width/2-150, self.screen_height/2+10, 300, 100, (255,255,255), (0,0,0,0), "Try Again", 40)
-        while self.run and not self.play :
-         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.run = False
+        Restart_button = Button(self.screen_width / 2 - 150, self.screen_height / 2 + 10, 300, 100, (255, 255, 255), (0, 0, 0, 0), "Try Again", 40)
+        while self.run and not self.play:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.run = False
 
+            mouse_pos = pygame.mouse.get_pos()
+            mouse_pressed = pygame.mouse.get_pressed()
 
-         mouse_pos = pygame.mouse.get_pos()
-         mouse_pressed = pygame.mouse.get_pressed()
-         
-         if Restart_button.isPressed(mouse_pos, mouse_pressed):
-             for sprite in self.all_sprites:
-                 sprite.kill()
-             self.new() 
-         self.screen.blit(self.bg, (0,0))
-         self.screen.blit(self.ground_image, (self.i,450))
-         self.screen.blit(self.ground_image,(self.screen_width+self.i,450))
-         self.all_sprites.draw(self.screen)
-         self.screen.blit(self.end_screen, (0,0))
-         self.screen.blit(self.font.render("SCORE:" + str(self.score//2), True, (255,255,0)), (self.screen_width/2-60, 10))
-         self.screen.blit(Restart_button.image, Restart_button.rect)
-         self.clock.tick(self.fps)
-         pygame.display.update()
+            if Restart_button.isPressed(mouse_pos, mouse_pressed):
+                for sprite in self.all_sprites:
+                    sprite.kill()
+                self.new() 
+            self.screen.blit(self.bg, (0, 0))
+            self.screen.blit(self.ground_image, (self.i, 450))
+            self.screen.blit(self.ground_image, (self.screen_width + self.i, 450))
+            self.all_sprites.draw(self.screen)
+            self.screen.blit(self.end_screen, (0, 0))
+            self.screen.blit(self.font.render("SCORE:" + str(self.score // 2), True, (255, 255, 0)), (self.screen_width / 2 - 60, 10))
+            self.screen.blit(Restart_button.image, Restart_button.rect)
+            self.clock.tick(self.fps)
+            pygame.display.update()
 
-
-
-
-
-
-
+# Klass för rör
 class Pipe(pygame.sprite.Sprite):
-   def __init__(self, game, pipe_x, pipe_y ):
+    def __init__(self, game, pipe_x, pipe_y):
         self.game = game
         self.groups = self.game.all_sprites, self.game.pipe_sprite
-        pygame.sprite.Sprite.__init__(self,self.groups)
+        pygame.sprite.Sprite.__init__(self, self.groups)
         self.x = pipe_x
         self.y = pipe_y
         self.width = 50
         self.height = 350
         self._layer = 10
-        self.image = pygame.surface.Surface((self.width,self.height), pygame.SRCALPHA)
-        self.image.blit(self.game.pipe, (0,0))
+        self.image = pygame.surface.Surface((self.width, self.height), pygame.SRCALPHA)
+        self.image.blit(self.game.pipe, (0, 0))
         self.rect = self.image.get_rect()
-        self.rect.x = self.x 
+        self.rect.x = self.x
         self.rect.y = self.y
         self.speed = 2
         self.i = 500
 
-        
-       
-
-   def update(self):
+    # Uppdatera rör
+    def update(self):
         self.movement()
         self.player_score()
-        
 
-   def movement(self):
-       self.rect.x -= self.speed
+    # Rörelselogik
+    def movement(self):
+        self.rect.x -= self.speed
+        if self.rect.x == 400:
+            self.game.pipe_new = True
 
-       if self.rect.x == 400:
-           self.game.pipe_new = True
+    # Poänglogik
+    def player_score(self):
+        if self.rect.x == 100:
+            self.game.score += 1
 
-
-   def player_score(self):
-       if self.rect.x == 100 :
-           self.game.score+=1
-           
- 
-
-      
-
- 
-        
-
+# Klass för spelarkaraktären
 class Player(pygame.sprite.Sprite):
-    def __init__(self, game, player_y, player_x ):
+    def __init__(self, game, player_y, player_x):
         self.game = game
         self.groups = self.game.all_sprites, self.game.player_sprite
-        pygame.sprite.Sprite.__init__(self,self.groups)
+        pygame.sprite.Sprite.__init__(self, self.groups)
         self.x = player_x
         self.y = player_y
         self.width = 50
         self.height = 50
         self._layer = 10
-        self.image = pygame.surface.Surface((self.width,self.height), pygame.SRCALPHA)
-        self.image.blit(self.game.sprite, (0,0))
+        self.image = pygame.surface.Surface((self.width, self.height), pygame.SRCALPHA)
+        self.image.blit(self.game.sprite, (0, 0))
         self.rect = self.image.get_rect()
-        self.rect.x = self.x 
+        self.rect.x = self.x
         self.rect.y = self.y
         self.speed = 0
         self.gravity = 5
-        
-        
 
+    # Uppdatera spelaren
     def update(self):
         self.movement()
         self.is_dead()
-        
-    
-    def movement(self):   
+
+    # Rörelselogik för spelaren
+    def movement(self):
         if pygame.key.get_pressed()[pygame.K_SPACE] and self.speed < 5:
-            self.speed = 20 
-        
-        if self.speed > 0.1: 
+            self.speed = 20
+
+        if self.speed > 0.1:
             self.rect.y -= self.speed
 
-        self.speed = self.speed * 0.90 
+        self.speed = self.speed * 0.90
         self.rect.y += self.gravity
-   
+
+    # Kontrollera om spelaren är död
     def is_dead(self):
         if self.rect.y > 400 or self.rect.y < 0:
             self.game.play = False
         if pygame.sprite.spritecollide(self, self.game.pipe_sprite, False):
-           self.game.play = False
-        
+            self.game.play = False
 
-    
-
-
-
-
+# Klass för knappar
 class Button:
-    def __init__(self, x, y, width, height, fg ,bg, content, font_size):
-        self.font = pygame.font.Font("comici.ttf",font_size)
+    def __init__(self, x, y, width, height, fg, bg, content, font_size):
+        self.font = pygame.font.Font("comici.ttf", font_size)
         self.content = content
 
         self.x = x
@@ -253,7 +242,7 @@ class Button:
         self.fg = fg
         self.bg = bg
 
-        self.image = pygame.Surface((self.width, self.height),pygame.SRCALPHA)
+        self.image = pygame.Surface((self.width, self.height), pygame.SRCALPHA)
         self.image.fill(self.bg)
         self.rect = self.image.get_rect()
 
@@ -261,9 +250,10 @@ class Button:
         self.rect.y = self.y
 
         self.text = self.font.render(self.content, True, self.fg)
-        self.text_rect = self.text.get_rect(center =(self.width/2, self.height/2))
+        self.text_rect = self.text.get_rect(center=(self.width / 2, self.height / 2))
         self.image.blit(self.text, self.text_rect)
     
+    # Kontrollera om knappen trycks
     def isPressed(self, pos, pressed):
         if self.rect.collidepoint(pos):
             if pressed[0]:
@@ -271,14 +261,11 @@ class Button:
             return False
         return False
 
-
-
+# Initiera och kör spelet
 g = Game()
-
 g.start()
 g.new()
 while g.run:
     g.main()
     g.end()
 pygame.quit()
-
